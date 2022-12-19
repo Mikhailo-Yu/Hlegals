eed8537875a4ccde1988143f1386ee9548da0e86
Mikhailo.Yu <miha.jewish@gmail.com>
2022-12-19 11:42:35 +0200

    виправлення

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,14 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  $(".services-menu__btn").on("click", function() {
+    $(this).toggleClass("-active");
+    $(".services-menu__list").stop(true, true).slideToggle(500);
+  });
+});
+
+
 
 // считали DOM элементы
 const headersList = document.querySelectorAll(".slider__header");
