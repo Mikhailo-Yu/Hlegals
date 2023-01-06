23e3be3bf2d2eca04b529d47d64326545be85a74
Mikhailo.Yu <miha.jewish@gmail.com>
2023-01-06 17:05:36 +0200

    виправлення після здачі

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,30 +31,30 @@ $(document).ready(function() {
 
 
 // считали DOM элементы
-const headersList = document.querySelectorAll(".slider__header");
-const notesList = document.querySelectorAll(".slider__note");
-const descriptionsList = document.querySelectorAll(".slider__description");
-const indicatorsList = document.querySelectorAll(".slider__indicator");
+// const headersList = document.querySelectorAll(".slider__header");
+// const notesList = document.querySelectorAll(".slider__note");
+// const descriptionsList = document.querySelectorAll(".slider__description");
+// const indicatorsList = document.querySelectorAll(".slider__indicator");
 
-let index = 0;
-let interval = 5000;
-let heightsArr = [];
-let heightMax = null;
+// let index = 0;
+// let interval = 5000;
+// let heightsArr = [];
+// let heightMax = null;
 
-setInterval(() => {
-  // снимаем классы active для первых элементов
-  headersList[index].classList.toggle("-active");
-  notesList[index].classList.toggle("-active");
-  indicatorsList[index].classList.toggle("-active");
-  // увеличиваем индекс, пока не превышено количество элементов
-  index = (index + 1) % headersList.length;
-  // ставим классы active следующим элементам
-  headersList[index].classList.toggle("-active");
-  notesList[index].classList.toggle("-active");
-  indicatorsList[index].classList.toggle("-active");
-}, interval);
+// setInterval(() => {
+//   // снимаем классы active для первых элементов
+//   headersList[index].classList.toggle("-active");
+//   notesList[index].classList.toggle("-active");
+//   indicatorsList[index].classList.toggle("-active");
+//   // увеличиваем индекс, пока не превышено количество элементов
+//   index = (index + 1) % headersList.length;
+//   // ставим классы active следующим элементам
+//   headersList[index].classList.toggle("-active");
+//   notesList[index].classList.toggle("-active");
+//   indicatorsList[index].classList.toggle("-active");
+// }, interval);
 
-// вычисление и изменение высоты блока описания под максимальный текст
-descriptionsList.forEach(el => heightsArr.push(el.clientHeight));
-heightMax = Math.max(...heightsArr);
-descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
+// // вычисление и изменение высоты блока описания под максимальный текст
+// descriptionsList.forEach(el => heightsArr.push(el.clientHeight));
+// heightMax = Math.max(...heightsArr);
+// descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
